db443945e03abcc9aab5463df74b98ec2c8c684c
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2021-03-01 01:13:49 +0100

    Fixed version config and header file.

---
+++ config/version.h
@@ -0,0 +1,10 @@
+#ifndef GENERAL_PURPOSE_CONTROL_VERSION_H
+#define GENERAL_PURPOSE_CONTROL_VERSION_H
+
+namespace gpc::CMake::Version {
+constexpr int major { @PROJECT_VERSION_MAJOR@ };
+constexpr int minor { @PROJECT_VERSION_MINOR@ };
+constexpr int patch { @PROJECT_VERSION_PATCH@ };
+}
+
+#endif // GENERAL_PURPOSE_CONTROL_VERSION_H
---
+++ include/version.h
@@ -0,0 +1,10 @@
+#ifndef GENERAL_PURPOSE_CONTROL_VERSION_H
+#define GENERAL_PURPOSE_CONTROL_VERSION_H
+
+namespace gpc::CMake::Version {
+constexpr int major { 1 };
+constexpr int minor { 0 };
+constexpr int patch { 0 };
+}
+
+#endif // GENERAL_PURPOSE_CONTROL_VERSION_H
